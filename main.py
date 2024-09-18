edec5d294d3e72caa578aa1cf5d63b0961f79af1
tathsaraniliyanage <prabodhathathsarani28@gmail.com>
2024-09-18 09:27:01 +0530

    WordList updated

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from textblob import TextBlob
+from textblob import TextBlob, WordList
+import nltk
+# nltk.download('punkt_tab')
 
 wiki = TextBlob("Python is a high-level, general-purpose programming language.")
 print(wiki)
 
-
-
-
+animals = TextBlob("cat dog octopus")
+animals.words
+WordList(['cat', 'dog', 'octopus'])
+animals.words.pluralize()
+WordList(['cats', 'dogs', 'octopodes'])
+print(animals)
+print(animals.words)
+print(animals.words.pluralize())
